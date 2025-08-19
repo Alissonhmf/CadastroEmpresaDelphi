a6dc2accbf8ace3c767e51add3b25385092926c3
Alisson Henrique <alissonhmf@gmail.com>
2025-08-19 18:05:39 -0300

    Primeiro commit, Telas e algumas funções prontas

---
+++ unBuscar.pas
@@ -0,0 +1,66 @@
+unit unBuscar;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.StdCtrls, Vcl.Grids,
+  Vcl.DBGrids, Vcl.ExtCtrls, Data.Win.ADODB;
+
+type
+  TForm2 = class(TForm)
+    Panel1: TPanel;
+    DBGrid1: TDBGrid;
+    btnAbrir: TButton;
+    Button1: TButton;
+    ADOQuery1: TADOQuery;
+    ADOConnection1: TADOConnection;
+    DataSource1: TDataSource;
+    procedure Button1Click(Sender: TObject);
+    procedure DBGrid1DblClick(Sender: TObject);
+  private
+    FSelecionado: Integer;
+  public
+    function SelecionarEmpresa: Integer;
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+uses unPrincipal;
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  Close;
+end;
+
+function TForm2.SelecionarEmpresa: Integer;
+begin
+  FSelecionado := -1;
+
+  if ShowModal = mrOk then
+    Result := FSelecionado
+  else
+    Result := -1;
+end;
+
+
+procedure TForm2.DBGrid1DblClick(Sender: TObject);
+begin
+  if not DBGrid1.DataSource.DataSet.IsEmpty then
+  begin
+    // Define o código selecionado
+    FSelecionado := DBGrid1.DataSource.DataSet.FieldByName('CODFILIAL').AsInteger;
+
+    // Seta o resultado modal para mrOk
+    ModalResult := mrOk;
+
+  end;
+end;
+
+
+
+end.
